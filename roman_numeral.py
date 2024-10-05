9283e5fded898c4065605f56412ce06d1ce892bb
goncalohgs <goncalohgs@gmail.com>
2024-10-05 18:15:40 +1000

    fix bug of Roman numeral made of two letters insted of one

--- roman_numeral.py
+++ roman_numeral.py
@@ -2,6 +2,25 @@ numeral_input = input("Enter the roman numeral you want to convert: ")
 
 def roman_to_int(numeral):
     final_answer = 0
+    if "CM" in numeral:
+        final_answer += 900
+        numeral = numeral.replace("CM", "")
+    if "CD" in numeral:
+        final_answer += 400
+        numeral = numeral.replace("CD", "")
+    if "XC" in numeral:
+        final_answer += 90
+        numeral = numeral.replace("XC", "")
+    if "XL" in numeral:
+        final_answer += 40
+        numeral = numeral.replace("XL", "")
+    if "IX" in numeral:
+        final_answer += 9
+        numeral = numeral.replace("IX", "")
+    if "IV" in numeral:
+        final_answer += 4
+        numeral = numeral.replace("IV", "")
+        
     for i in numeral:
         if i == "M":
             final_answer += 1000
